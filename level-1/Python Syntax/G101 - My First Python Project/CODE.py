e8a5889aad5022936d7bac76c35c060b0fb1e81a
Jagrat Ahuja <90457218+jagratahuja@users.noreply.github.com>
2026-03-15 19:55:17 +0530

    Add CODE.py for Python syntax exercises

---
+++ level-1/Python Syntax/G101 - My First Python Project/CODE.py
@@ -0,0 +1,30 @@
+# Program ID: G101
+# Exercise Type: Python Syntax / Print Statements
+# Tasks Included:
+# - Task 1: Print a greeting message
+# - Task 2: Print "Hello World"
+# - Task 3: Print a sentence about learning Python
+# - Task 4: Print definitions of program and software
+
+# Original code starts below
+
+#Level 1
+#Part-1 Python Syntax
+#G101 - My First Python Project.py
+''' Task 1: '''
+print("***** Task 1: *****")
+print('Hey Python')
+
+
+''' Task 2: '''
+print("***** Task 2: *****")
+print('Hello World')
+
+''' Task 3: '''
+print("***** Task 3: *****")
+print("I am excited for my first class on python.")
+
+''' Task 4: '''
+print("***** Task 4: *****")
+print("A computer program's is a set of commands that you tell the hardware what to do")
+print("Software is a collection of computer programs.")
