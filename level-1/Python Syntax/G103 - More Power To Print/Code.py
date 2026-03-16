4224c38b392b54025f7bb8283496f3e5bc5cbea5
Jagrat Ahuja <90457218+jagratahuja@users.noreply.github.com>
2026-03-17 00:27:06 +0530

    Add print statements and string concatenation tasks for G103
    This program demonstrates various print statements and string concatenation tasks, including jokes and formatted output.

---
+++ level-1/Python Syntax/G103 - More Power To Print/Code.py
@@ -0,0 +1,68 @@
+# Program ID: G103
+# Exercise Type: Print Statements and String Concatenation
+# Tasks Included:
+# - Task 1: Print a joke with a blank line
+# - Task 2: Print another joke with formatted spacing
+# - Task 3: Demonstrate string concatenation
+# - Task 4: Concatenate and print a full name
+# - Task 5: Print a simple joke
+
+# Original code starts below
+
+# Level 1
+# Part-1 Python Syntax
+# G103 - More Power To Print
+print("***** Task 1: *****")
+
+print("What did the spider do on the computer?")
+print() # gives a blank line
+print ("Made a website!")
+
+
+''' Task 2: '''
+print("***** Task 2: *****")
+print("Why did Jack and Jill climb up the mountain?")
+print()
+print("To get bettter signals of wifi")
+
+''' Task 3: '''
+print("***** Task 3: *****")
+
+print ("This is " + "a good string")
+
+''' Task 4: '''
+print("***** Task 4: *****")
+print("Jagrat " + "Ahuja")
+
+''' Task 5: '''
+print("***** Task 5: *****")
+
+print("How do you make a hot dog stand?")
+print("You take away its chair!")
+print("***** Task 1: *****")
+
+print("What did the spider do on the computer?")
+print() # gives a blank line
+print ("Made a website!")
+
+
+''' Task 2: '''
+print("***** Task 2: *****")
+print("Why did Jack and Jill climb up the mountain?")
+print()
+print("To get bettter signals of wifi")
+
+''' Task 3: '''
+print("***** Task 3: *****")
+
+print ("This is " + "a good string")
+
+''' Task 4: '''
+print("***** Task 4: *****")
+print("Jagrat " + "Ahuja")
+
+''' Task 5: '''
+print("***** Task 5: *****")
+
+print("How do you make a hot dog stand?")
+print("You take away its chair!")
