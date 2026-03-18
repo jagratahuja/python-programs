4a392aeb40c9fb2c6203e5a8a66e90df3dc60373
Jagrat Ahuja <90457218+jagratahuja@users.noreply.github.com>
2026-03-18 11:59:12 +0530

    Add Level 1 Python syntax G104 program in Code.py

---
+++ level-1/Python Syntax/G104 - About Me/Code.py
@@ -0,0 +1,17 @@
+# Program ID: G104
+# Exercise Type: Basic Output / Personal Information Display
+# Tasks Included:
+# - Task 1: Print sentences describing personal details and interests
+
+# Original code starts below
+
+# Level 1
+# Part 1 - Python Syntax
+# G104 - About Me
+''' Task 1: '''
+print("***** Task 1: *****")
+print("I study in __ grade.")
+print("My favourite subject is maths.")
+print("My favorite language is English.")
+print("I like to code and read.")
+print("I am interested in computer languages, that is the reason I like coding.") 
