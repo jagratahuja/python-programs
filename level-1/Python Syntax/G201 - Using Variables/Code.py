6ecb2776ebb171822bd060576125609b63fe6b40
Jagrat Ahuja <90457218+jagratahuja@users.noreply.github.com>
2026-03-19 21:20:57 +0530

    Add Python script for variable exercises
    This script demonstrates variable declaration and basic output in Python, covering tasks such as greeting, storing favorite color and number, and examples of invalid variable names.

---
+++ level-1/Python Syntax/G201 - Using Variables/Code.py
@@ -0,0 +1,66 @@
+# Program ID: G201
+# Exercise Type: Variables and Basic Output
+# Tasks Included:
+# - Task 1: Declare a variable and print a greeting
+# - Task 2: Store and print favorite color and number
+# - Task 3: Store a number in a variable and print it
+# - Task 4: Examples of invalid variable names (commented)
+# - Task 5: Store and print a date using variables
+
+# Original code starts below
+
+# Level 1
+# Part 1: Python Syntax
+# G201 - Using Variables
+''' Task 1: '''
+print("")
+print("***** Task 1: *****")
+
+NAME = "Sam"
+print("Hello " + NAME)
+
+
+''' Task 2: '''
+print("")
+print("***** Task 2: *****")
+
+#declare and display your favorite color here
+Color= "Red"
+print("My favourite color is " + Color)
+NUM=10
+print("My Favourite Number is",NUM) 
+
+
+''' Task 3: '''
+print("")
+print("***** Task 3: *****")
+
+C = 5
+print (C)
+
+
+''' Task 4: '''
+print("")
+print("***** Task 4: *****")
+
+# 007bond = 'big car' 
+#Hint: invalid syntax (starts with a number)
+
+# more@ = 1000000
+#Hint: invalid syntax (Has a special character)
+
+# class = 'noise makers'
+#Hint: invalid syntax (class is a keyword)
+
+#print(trouble)
+
+
+''' Task 5: '''
+print("")
+print("***** Task 5: *****")
+
+todays_date = 24
+todays_month = 3
+todays_year = 2021
+
+print(todays_date,todays_month,todays_year)
