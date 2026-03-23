08b684327ea337d7941b058ca631e9796aea1b5f
Jagrat Ahuja <90457218+jagratahuja@users.noreply.github.com>
2026-03-23 14:34:41 +0530

    Add Code.py for Level 1 Part 1 G205 - Swapping Numbers
    This program accepts user input for first name and surname, prints a greeting, and demonstrates variable swapping using a temporary variable.

---
+++ level-1/Python Syntax/G205 - Swapping Numbers/Code.py
@@ -0,0 +1,31 @@
+# Program ID: G205
+# Exercise Type: User Input and Variable Swapping
+# Tasks Included:
+# - Task 1: Accept first name and surname and print a greeting
+# - Task 2: Swap the values of two variables using a temporary variable
+
+# Original code starts below
+
+# Level 1
+# Part 1: Python Syntax
+# G205 - Swapping Numbers
+''' Task 1: '''
+print(" ")
+print("***** Task 1: *****")
+
+NAME = input("Enter your First name ")
+Surname = input("Enter your second name ")
+print("Hello " + NAME + " " + Surname)
+
+
+print("Hello " + NAME + " " + Surname)
+
+a = 5
+b = 7
+print(a)
+print(b)
+temp = a
+a = b
+b = temp
+print(a)
+print(b)
