fa6da197313d0d8a5147799d555265f04a4cca77
Jagrat Ahuja <90457218+jagratahuja@users.noreply.github.com>
2026-03-26 21:46:27 +0530

    Add Level 1 Python Syntax G303 program
    Added a temperature conversion program that converts between Celsius and Fahrenheit.

---
+++ level-1/Python Syntax/G303 - Temperature Convertor/Code.py
@@ -0,0 +1,18 @@
+# Program ID: G303
+# Exercise Type: Temperature Conversion using Arithmetic Expressions
+# Tasks Included:
+# - Task 1: Convert temperature from Celsius to Fahrenheit
+# - Task 2: Convert temperature from Fahrenheit to Celsius
+
+# Original code starts below
+
+# Level 1
+# Part 1: Python Syntax
+# G303 - Temperature Convertor
+fahrenheit=int(input("Enter the unit in Celsius : "))
+celsius=(fahrenheit * 9/5) + 32
+print("The same value in Fahrenheit is",celsius)
+print(" ")
+cel=int(input("Enter the unit in Fahrenheit : "))
+fah=(cel - 32) * 5/9
+print("The same value in Celsius is",fah)
